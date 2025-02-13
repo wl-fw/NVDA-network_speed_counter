4b2514217907cd365d30a31c9e70be7666abbe95
wl-fw <wallandacosta@gmail.com>
2025-02-13 13:29:19 -0300

    release2

--- buildVars.py
+++ buildVars.py
@@ -36,15 +36,18 @@ It can span multiple lines."""),
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": 2024.3.0,
+	"addon_minimumNVDAVersion": 2024.3,
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": 2025.1.0,
+	"addon_lastTestedNVDAVersion": 2025.1,
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": Beta,
+"addon_updateChannel": "beta",
+
+
 	# Add-on license such as GPL 2
-	"addon_license": GNU GPL 2.0,
+"addon_license": "GPL-2.0",
+
 	# URL for the license document the ad-on is licensed under
 	"addon_licenseURL": None,
 }
